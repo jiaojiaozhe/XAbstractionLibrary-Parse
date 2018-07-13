5500793ec1296462958d76a1bce664aa6c030ec1
jiaojiaozhe <bluebiao@163.com>
2018-07-13 14:09:09 +0800

    Initial commit

---
+++ Example/XAbstractionLibrary-Parse/XAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XAppDelegate.h
+//  XAbstractionLibrary-Parse
+//
+//  Created by jiaojiaozhe on 07/13/2018.
+//  Copyright (c) 2018 jiaojiaozhe. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XAbstractionLibrary-Parse/XViewController.h
@@ -0,0 +1,13 @@
+//
+//  XViewController.h
+//  XAbstractionLibrary-Parse
+//
+//  Created by jiaojiaozhe on 07/13/2018.
+//  Copyright (c) 2018 jiaojiaozhe. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XViewController : UIViewController
+
+@end
